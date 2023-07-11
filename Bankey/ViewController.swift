ae7c1f2887849f9e9a37ee4fa371e03e5f434148
Vladimir Fibe <motiw@icloud.com>
2023-07-11 11:46:17 +0600

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by MacService on 7/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
