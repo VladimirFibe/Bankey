3799a740d447ed7ee12624f6a0ada52df51ebfdb
Vladimir Fibe <motiw@icloud.com>
2023-07-11 16:34:15 +0600

    save login

--- Bankey/LoginViewController.swift
+++ Bankey/LoginViewController.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
     let loginView = LoginView()
     let signInButton = UIButton(type: .system)
     let errorMessageLabel = UILabel()
+    
+    var username: String? {
+        loginView.usernameTextField.text
+    }
+    
+    var password: String? {
+        loginView.passwordTextField.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -12,7 +22,28 @@ class LoginViewController: UIViewController {
 // MARK: - Actons
 extension LoginViewController {
     @objc private func signInTapped() {
-        errorMessageLabel.isHidden.toggle()
+        errorMessageLabel.isHidden = true
+        signInButton.configuration?.showsActivityIndicator = false
+        login()
+    }
+    
+    private func login() {
+        guard let username, let password else {
+            assertionFailure("Username / password should never be nil")
+            return
+        }
+        if username.isEmpty || password.isEmpty {
+            configure(withMessage: "Username / password cannot be blank")
+        } else if username == "Kevin" && password == "Welcome" {
+            signInButton.configuration?.showsActivityIndicator = true
+        } else {
+            configure(withMessage: "Incorrect username / password")
+        }
+    }
+    
+    private func configure(withMessage message: String) {
+        errorMessageLabel.text = message
+        errorMessageLabel.isHidden = false
     }
 }
 
@@ -23,8 +54,43 @@ extension LoginViewController {
         setupLoginView()
         setupSignInButton()
         setupErrorMessageLabel()
+        setupSubtitleLabel()
+        setupTitleLabel()
     }
     
+    private func setupTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        titleLabel.font = .preferredFont(forTextStyle: .largeTitle)
+        titleLabel.adjustsFontForContentSizeCategory = true
+        titleLabel.text = "Bankey"
+        
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 2),
+            titleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+        
+    }
+    
+    private func setupSubtitleLabel() {
+        view.addSubview(subtitleLabel)
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.numberOfLines = 0
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.font = .preferredFont(forTextStyle: .title3)
+        subtitleLabel.adjustsFontForContentSizeCategory = true
+        subtitleLabel.text = "Your premium source for all things banking!"
+        
+        NSLayoutConstraint.activate([
+            loginView.topAnchor.constraint(equalToSystemSpacingBelow: subtitleLabel.bottomAnchor, multiplier: 2),
+            subtitleLabel.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
+            subtitleLabel.trailingAnchor.constraint(equalTo: loginView.trailingAnchor)
+        ])
+        
+    }
     private func setupLoginView() {
         view.addSubview(loginView)
         NSLayoutConstraint.activate([
@@ -55,7 +121,6 @@ extension LoginViewController {
         errorMessageLabel.textAlignment = .center
         errorMessageLabel.textColor = .systemRed
         errorMessageLabel.numberOfLines = 0
-        errorMessageLabel.text = "Error failure"
         errorMessageLabel.isHidden = true
         
         NSLayoutConstraint.activate([
