9e50486749280d5cf7a573bdad817261b1f7720b
Vladimir Fibe <motiw@icloud.com>
2023-07-15 15:38:42 +0600

    feat: Add CurrencyFormatter

--- Bankey/Feature/Account Summary/Cells/AccountSummaryCell.swift
+++ Bankey/Feature/Account Summary/Cells/AccountSummaryCell.swift
@@ -28,7 +28,12 @@ final class AccountSummaryCell: UITableViewCell {
         let accountType: AccountType
         let accountName: String
         let balance: Decimal
+        
+        var balanceAsAttributedString: NSAttributedString {
+            CurrencyFormatter().makeAttributedCurrency(balance)
+        }
     }
+    
     static let id = "AccountSummaryCell"
     static let rowHeight: CGFloat = 112
     var viewModel: ViewModel? = nil
@@ -119,7 +124,6 @@ extension AccountSummaryCell {
     private func setupBalanceAmountLabel() {
         balanceStackView.addArrangedSubview(balanceAmountLabel)
         balanceAmountLabel.textAlignment = .right
-        balanceAmountLabel.attributedText = makeFormattedBalance(dollars: "926,466", cents: "23")
     }
     
     private func setupChevronImageView() {
@@ -132,26 +136,6 @@ extension AccountSummaryCell {
             chevronImageView.leadingAnchor.constraint(equalToSystemSpacingAfter: balanceStackView.trailingAnchor, multiplier: 2)
         ])
     }
-    
-    private func makeFormattedBalance(dollars: String, cents: String) -> NSMutableAttributedString {
-        let dollarSignAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.preferredFont(forTextStyle: .callout),
-            .baselineOffset: 8]
-        let dollarAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.preferredFont(forTextStyle: .title1)]
-        let centAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.preferredFont(forTextStyle: .footnote),
-            .baselineOffset: 8]
-
-        let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
-        let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
-        let centString = NSAttributedString(string: cents, attributes: centAttributes)
-        
-        rootString.append(dollarString)
-        rootString.append(centString)
-        
-        return rootString
-    }
 }
 
 extension AccountSummaryCell {
@@ -160,5 +144,65 @@ extension AccountSummaryCell {
         nameLabel.text = viewModel.accountName
         underlineView.backgroundColor = viewModel.accountType.color
         balanceLabel.text = viewModel.accountType.balanceLabel
+        balanceAmountLabel.attributedText = viewModel.balanceAsAttributedString
+    }
+}
+
+struct CurrencyFormatter {
+    func makeAttributedCurrency(_ amount: Decimal) -> NSMutableAttributedString {
+        let tuple = breakIntoDollarsAndCents(amount)
+        return makeFormattedBalance(dollars: tuple.0, cents: tuple.1)
+    }
+    
+    private func breakIntoDollarsAndCents(_ amount: Decimal) -> (String, String) {
+        let double = NSDecimalNumber(decimal: amount).doubleValue
+        let tuple = modf(double)
+        let dollars = convertDollar(tuple.0)
+        let cents = convertCents(tuple.1)
+        return (dollars, cents)
+    }
+    
+    private func convertDollar(_ dollarPart: Double) -> String {
+        let dollarsWithDecimal = dollarsFormatted(dollarPart)
+        let formatter = NumberFormatter()
+        let decimalSeparator = formatter.decimalSeparator!
+        let dollarComponents = dollarsWithDecimal.components(separatedBy: decimalSeparator)
+        var dollars = dollarComponents.first!
+        dollars.removeFirst()
+        return dollars
+    }
+    
+    private func convertCents(_ centPart: Double) -> String {
+        let cent = (centPart * 100.0).rounded()
+        return String(format: "%.02d", Int(cent))
+    }
+    
+    private func dollarsFormatted(_ dollars: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.numberStyle = .currency
+        formatter.usesGroupingSeparator = true
+        if let result = formatter.string(from: dollars as NSNumber) {
+            return result
+        } else {
+            return ""
+        }
+    }
+    
+    private func makeFormattedBalance(dollars: String, cents: String) -> NSMutableAttributedString {
+        let dollarSignAttributes: [NSAttributedString.Key: Any]
+        = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
+        let dollarAttributes: [NSAttributedString.Key: Any]
+        = [.font: UIFont.preferredFont(forTextStyle: .title1)]
+        let centAttributes: [NSAttributedString.Key: Any]
+        = [.font: UIFont.preferredFont(forTextStyle: .footnote), .baselineOffset: 8]
+        
+        let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
+        let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
+        let centString = NSAttributedString(string: cents, attributes: centAttributes)
+        rootString.append(dollarString)
+        rootString.append(centString)
+        
+        return rootString
     }
 }
