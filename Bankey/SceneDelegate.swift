883797b3adc778486f58218d08735a2fbeb50a11
Almaty <almaty@Mac.lan>
2025-06-28 15:17:05 +0500

    start

---
+++ Bankey/SceneDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let winodwScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: winodwScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
