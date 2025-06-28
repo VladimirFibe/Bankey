12a66c44fdf9896a1edc78f1789cb30f660c1ce6
Almaty <almaty@Mac.lan>
2025-06-28 23:55:02 +0500

    cornerRadius

--- Bankey/Login/LoginViewController.swift
+++ Bankey/Login/LoginViewController.swift
@@ -5,7 +5,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
         style()
         layout()
     }
