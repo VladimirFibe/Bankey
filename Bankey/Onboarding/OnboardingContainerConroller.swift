f3d4e51006962c2ff8f912c25fe0527117e54c9a
Vladimir <motiw@me.com>
2025-06-29 11:24:16 +0500

    Pages Controller ready

--- Bankey/Onboarding/OnboardingContainerConroller.swift
+++ Bankey/Onboarding/OnboardingContainerConroller.swift
@@ -2,8 +2,23 @@ import UIKit
 
 final class OnboardingContainerConroller: UIViewController {
     private let pageViewController: UIPageViewController
+    private var pages = [UIViewController]()
+    var currentVC: UIViewController {
+        didSet {
+            
+        }
+    }
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        
+        let first = FirstViewController()
+        let second = SecondViewController()
+        let third = ThirdViewController()
+        pages.append(first)
+        pages.append(second)
+        pages.append(third)
+        currentVC = pages.first!
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
@@ -17,14 +32,46 @@ final class OnboardingContainerConroller: UIViewController {
         addChild(pageViewController)
         pageViewController.didMove(toParent: self)
         view.addSubview(pageViewController.view)
-//        pageViewController.dataSource = self
+        pageViewController.dataSource = self
         pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false)
+        currentVC = pages.first!
         NSLayoutConstraint.activate([
             view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
             view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
             view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
             view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor)
         ])
+    }
+}
+
+extension OnboardingContainerConroller: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return getPreviousViewController(from: viewController)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return getNextViewController(from: viewController)
+    }
+
+    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+        currentVC = pages[index - 1]
+        return pages[index - 1]
+    }
+
+    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
+        currentVC = pages[index + 1]
+        return pages[index + 1]
+    }
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pages.firstIndex(of: self.currentVC) ?? 0
     }
 }
