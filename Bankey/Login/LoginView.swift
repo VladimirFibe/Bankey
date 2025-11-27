ccca84f707c77a2614c6df16c2ab2c963b846516
Владимир Файб <gatsby@Mac.lan>
2025-11-27 10:57:24 +0500

    Add usernameTextField

--- Bankey/Login/LoginView.swift
+++ Bankey/Login/LoginView.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 final class LoginView: UIView {
+    private let usernameTextField = UITextField()
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
@@ -20,9 +21,33 @@ private extension LoginView {
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemOrange
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self
     }
     
     func layout() {
+        addSubview(usernameTextField)
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            usernameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: usernameTextField.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        textField.text != "" ? true : false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
         
     }
 }
--- Bankey/Login/LoginViewController.swift
+++ Bankey/Login/LoginViewController.swift
@@ -26,3 +26,4 @@ private extension LoginViewController {
     }
 }
 
+
